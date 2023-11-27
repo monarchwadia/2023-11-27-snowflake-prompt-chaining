cca8f724a0e1c7a25bc4c4da56d082c6fd2edba5
Monarch Wadia <monarchwadia@gmail.com>
2023-11-27 16:38:23 -0500

    Updated main and util to be cleaner

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from dotenv import load_dotenv
-
-from util import inference
 load_dotenv()
-from openai import OpenAI
-from openai.types.chat import ChatCompletionMessage
-from pathlib import Path
 
-l2_prompt = inference("Level 3 prompt: Create a clock in HTML5/CSS/JavaScript.")
-l1_prompt = inference(f"Level 2 prompt: {l2_prompt}")
-result = inference(f"Level 1 prompt: {l1_prompt}")
-Path('./.result.md').write_text(result)
+from util import RunManager
+
+rm = RunManager()
+l2_prompt = rm.inference("Level 3 prompt: Create a clock in HTML5/CSS/JavaScript.")
+l1_prompt = rm.inference(f"Level 2 prompt: {l2_prompt}")
+result = rm.inference(f"Level 1 prompt: {l1_prompt}")
 print(result)
+rm.save_result(result)
--- util.py
+++ util.py
@@ -5,6 +5,7 @@ from openai.types.chat import ChatCompletionMessage
 from pathlib import Path
 import os
 import logging
+from datetime import datetime
 
 # Constants & configurations
 L4_PROMPT = Path('./prompt.md').read_text()
@@ -13,21 +14,48 @@ logging.basicConfig(filename='.log', encoding='utf-8', level=logging.INFO)
 # Variables
 messages: list[ChatCompletionMessage] = []
 
-# Functions
-def inference(prompt: str) -> ChatCompletionMessage:
-    logging.info(f"inference ran with prompt:\n====================\n{prompt}\n====================")
-    global messages
-    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages + [
-            {"role": "system", "content": L4_PROMPT},
-            {"role": "user", "content": prompt}
-        ]
-    )
-    returned_message = completion.choices[0].message
-    messages = messages + [returned_message]
-    content = returned_message.content
-    logging.info(f"API returned with content:\n====================\n{content}\n====================")
-    return content
+class RunManager:
+    """
+    A class that manages the running of the program.
+    It outputs logs and results to files which match the current time.
+    Logs are logged in `.output/YYYY-MM-DD-HH-MM-SS/.log`
+    Results are saved in `.output/YYYY-MM-DD-HH-MM-SS/.result.md`
+    """
+
+    def __init__(self):
+        # get current time in YYYY-MM-DD-HH-MM-SS format
+        self.time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        self.logger = logging.getLogger(self.time)
+        # ensure the output directory exists
+        Path(f'.output/{self.time}').mkdir(parents=True, exist_ok=True)
+
+        # configure logger to output to file
+        handler = logging.FileHandler(f'.output/{self.time}/.log')
+        handler.setLevel(logging.INFO)
+        self.logger.addHandler(handler)
+        # configure result to output to file
+        self.result_path = Path(f'.output/{self.time}/.result.md')
+        
+    def inference(self, prompt: str) -> ChatCompletionMessage:
+        self.logger.info(f"inference ran with prompt:\n====================\n{prompt}\n====================")
+        global messages
+        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages + [
+                {"role": "system", "content": L4_PROMPT},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        returned_message = completion.choices[0].message
+        messages = messages + [returned_message]
+        content = returned_message.content
+        self.logger.info(f"API returned with content:\n====================\n{content}\n====================")
+        return content
     
+    def save_result(self, result: str):
+        self.result_path.write_text(result)
+        self.logger.info(f"Result saved to {self.result_path}")
+
+# Functions
+
